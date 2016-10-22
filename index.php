8cfc415d4de01bde638b231915b9652b71b8fce6
Charles Westerby <cwesterby@gmail.com>
2016-10-22 20:37:40 +0100

    live search added

--- index.php
+++ index.php
@@ -54,99 +54,181 @@
     <![endif]-->
   </head>
   <body>
-    <div class="search">
-      <h1>Search</h1>
-      <input type="search" id="searchField" value="Search a word">
-      <button id="toggleVerse" href="#">Search a word</button>
+    <div class="container">
+      <div class="search">
+        <h1>Search</h1>
+        <input type="search" id="searchField" value="">
+      </div>
+
+      <div class="results">
+        <h1>Results</h1>
+        <table class="table"></table>
+      </div>
+
+      <div class="input">
+        <h1>Input</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <input type="text" name="welsh_word" class="form-control" id="welsh_word" placeholder="welsh">
+          <input type="text" name="english_word" class="form-control" id="english_word" placeholder="english">
+          <button type="submit" name="submit_button" class="btn btn-default white" value="submit">Submit</button>
+        </form>
+      </div>
+
     </div>
 
-    <div class="results">
-      <h1>Results</h1>
-      <div name="jam" class="output"></div>
-      <table class="table"><table>
-    </div>
-
-    <div class="input">
-      <h1>Input</h1>
-      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <input type="text" name="welsh_word" class="form-control" id="welsh_word" placeholder="welsh">
-        <input type="text" name="english_word" class="form-control" id="english_word" placeholder="english">
-        <button type="submit" name="submit_button" class="btn btn-default white" value="submit">Submit</button>
-      </form>
-    </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="assets/jquery/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="assets/js/bootstrap.min.js"></script>
     <script type="text/javascript">
+      // array of the alphabet, used to create the headings
       alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+      // begin once the page loads
       $(document).ready(function() {
-        //Gets the all verses from the PHP JSON file sets it to products variable
+
+        // Gets the all verses from the PHP JSON file sets it to products variable,
+        // which is at the beginning of this file
         var $allWords = JSON.parse('<?php echo json_encode($allWords) ?>');
 
+        // turns the $allWords object into an array so the words can be sorted
         var allWords = [];
-        
         $.each($allWords , function( index, value ) {
           allWords[index] = {id:index, welsh_word:value.welsh_word, english_word:value.english_word};
           // console.log($allWords[index]);
         });
 
-        //// sorts the array by welsh_word
+        // Sorts the array by welsh_word
         allWords.sort(function(a, b){
             if(a.welsh_word < b.welsh_word) return -1;
             if(a.welsh_word > b.welsh_word) return 1;
             return 0;
         });
 
-        // Search while Typing
-      	$('#searchField').bind('input propertychange', function () {
-      		var lookup = $('#searchField').val().replace(' ','+');
-      		if (lookup!==''&&lookup.length>1) {
-      			get(lookup);
+        // outputs all the words in the database into the results div
+        var Sparent = $('.results');
+        outputResultsTable(alphabet,allWords,Sparent);
+
+        // Search while typing. Each keystroke in the search bar triggers
+        // the function. The string from the search bar is used to search
+        // the allwords array. results are then formatted and appended to results.
+        // if the search bar is empty all the words output to the results div
+      	$('#searchField').bind('keyup', function () {
+      		var lookup = $('#searchField').val();
+          lookup = lookup.toLowerCase();
+      		if (lookup!==''&&lookup.length>=1) {
+      			get(lookup, allWords, alphabet);
+      		} else {
+            $('.results table').slice(1).remove();
+            var Sparent = $('.results');
+            outputResultsTable(alphabet,allWords,Sparent)
       		}
       	});
 
-        // Search the array for matching strings
-        function get(lookup) {
+        // get fundtion used in the 'keyup' action
+        // children of results are removed first
+        function get(lookup, allWords, alphabet) {
           // console.log(lookup);
+          $('.results table').slice(1).remove();
+          var Sparent = $('.results');
 
+          result = searchFor(lookup, allWords);
+          outputResultsTable(alphabet,result,Sparent);
+        }
+        // loops through the array to find the matching values
+        function searchFor(toSearch, objects) {
+          var result = [];
+          toSearch = trimString(toSearch); // trim it
+          for(var i=0; i<objects.length; i++) {
+            for(var key in objects[i]) {
+              if(objects[i][key].toLowerCase().indexOf(toSearch)!=-1) {
+                if(!itemExists(result, objects[i])) result.push(objects[i]);
+              }
+            }
+          }
+          return result;
         }
 
+        // the 3 functions trim code, code to ensure no duplicates in result set
+        function trimString(s) {
+          var l=0, r=s.length -1;
+          while(l < s.length && s[l] == ' ') l++;
+          while(r > l && s[r] == ' ') r-=1;
+          return s.substring(l, r+1);
+        }
+        function compareObjects(o1, o2) {
+          var k = '';
+          for(k in o1) if(o1[k] != o2[k]) return false;
+          for(k in o2) if(o1[k] != o2[k]) return false;
+          return true;
+        }
+        function itemExists(haystack, needle) {
+          for(var i=0; i<haystack.length; i++) if(compareObjects(haystack[i], needle)) return true;
+          return false;
+        }
+
+        // result = searchFor(lookup, allWords);
+        // console.log(result);
+
+        // outputs the results into a table
+        function outputResultsTable(alphabet_array, wordsArray, parent ){
+          $.each(alphabet_array, function(index, value){
+            var theParent = parent;
+            var $lastTable = theParent.children("table[class*='table']").last();
+            var query = value;
+            $lastTable.after('<table class="table_'+value+'"><tr><th>'+value+'</th><th></th><th></th></tr>');
+            var $new_table = theParent.children("table[class='table_"+value+"']");
+
+            for(var key in wordsArray) {
+                if (wordsArray.hasOwnProperty(key)) {
+                    var y = wordsArray[key].welsh_word.charAt(0)
+                    y = y.toUpperCase();
+
+                    if (y != query) {
+                      // do nothing
+                    } else {
+                      $new_table.append('<tr><td>'+wordsArray[key].welsh_word+'</td><td><---></td><td>'+wordsArray[key].english_word+'</td></tr>');
+                    }
+                }
+            }
+
+            $new_table.append('</table>');
+          }); // end of each loop
+        } // end of outputResultsTable
 
 
-        // console.log(allWords);
-        //// print out the array
-        // for(var key in allWords) {
-        //     if (allWords.hasOwnProperty(key)) {
-        //         $output.append(allWords[key].welsh_word);
-        //         $output.append('  <->  ');
-        //         $output.append(allWords[key].english_word);
-        //         $output.append('<br>');
-        //     }
-        // }
+        function outputResultsTable2(alphabet_array, wordsArray, parent ){
+          $.each(alphabet_array, function(index, value){
+            var theParent = parent;
+            var $lastTable = theParent.children("table[class*='table']").last();
+            var query = value;
 
-        $.each(alphabet, function(index, value){
-          var $parent = $('.results');
-          var $lastTable = $parent.children("table[class*='table']").last();
-          var query = value;
-          $lastTable.after('<table class="table_'+value+'"><tr><th>'+value+'</th><th></th><th></th></tr>');
-          var $new_table = $parent.children("table[class='table_"+value+"']");
 
-          for(var key in allWords) {
-              if (allWords.hasOwnProperty(key)) {
-                  var y = allWords[key].welsh_word.charAt(0)
-                  y = y.toUpperCase();
+            for(var key in wordsArray) {
+                if (wordsArray.hasOwnProperty(key)) {
+                    var y = wordsArray[key].welsh_word.charAt(0)
+                    y = y.toUpperCase();
 
-                  if (y != query) {
-                    // do nothing
-                  } else {
-                    $new_table.append('<tr><td>'+allWords[key].welsh_word+'</td><td><---></td><td>'+allWords[key].english_word+'</td></tr>');
-                  }
-              }
-          }
-          $new_table.append('</table>');
-        });
+                    if (y != query) {
+                      // do nothing
+                    } else {
+                      $lastTable.after('<table class="table_'+value+'"><tr><th>'+value+'</th><th></th><th></th></tr>');
+                      var $new_table = theParent.children("table[class='table_"+value+"']");
+                      $new_table.append('<tr><td>'+wordsArray[key].welsh_word+'</td><td><---></td><td>'+wordsArray[key].english_word+'</td></tr>');
+                    }
+                }
+            }
+
+            $new_table.append('</table>');
+          }); // end of each loop
+        } // end of outputResultsTable
+
+
+
+
+
+
 
       });
     </script>
