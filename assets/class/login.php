73257104aae5cafb3b9f70af24ebe363e7e17248
Charles Westerby <cwesterby@gmail.com>
2016-10-27 22:52:18 +0100

    secure password details for heroku

--- assets/class/login.php
+++ assets/class/login.php
@@ -7,9 +7,4 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-// $host = 'us-cdbr-iron-east-04.cleardb.net';
-// $db = 'heroku_6d45d6c1715d1f6';
-// $username = 'bf992f4c41b84b';
-// $password = '1afd34fa';
-
 ?>
